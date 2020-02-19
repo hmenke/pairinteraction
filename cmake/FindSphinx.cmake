55623eca2b3a8c33a168192eafa131ec55a882ec
Henri Menke <henri@icp.uni-stuttgart.de>
2020-02-19 17:43:18 +1300

    [sphinx] Newer version moved some stuff around
    sphinx.main -> sphinx.cmd.build.main
    sphinx.apidoc.main -> sphinx.ext.apidoc.main

--- cmake/FindSphinx.cmake
+++ cmake/FindSphinx.cmake
@@ -4,22 +4,27 @@ if(NOT PYTHON_EXECUTABLE)
   find_package(PythonInterp 3)
 endif()
 
+function(FIND_SPHINX_LIBRARY name library)
+  if(NOT SPHINX_${name}_FOUND)
+    execute_process(
+      COMMAND "${PYTHON_EXECUTABLE}" -c
+      "from ${library} import main; print('TRUE')"
+      OUTPUT_VARIABLE FOUND
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      ERROR_QUIET)
+    set("SPHINX_${name}_FOUND" ${FOUND} PARENT_SCOPE)
+    set("SPHINX_${name}_LIBRARY" ${library} PARENT_SCOPE)
+  endif()
+endfunction()
+
 if(PYTHON_EXECUTABLE)
   # find sphinx-build
-  execute_process(
-    COMMAND "${PYTHON_EXECUTABLE}" -c
-    "import sphinx; print('TRUE')"
-    OUTPUT_VARIABLE SPHINX_BUILD_FOUND
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    ERROR_QUIET)
+  FIND_SPHINX_LIBRARY("BUILD" "sphinx")
+  FIND_SPHINX_LIBRARY("BUILD" "sphinx.cmd.build")
 
   # find sphinx-apidoc
-  execute_process(
-    COMMAND "${PYTHON_EXECUTABLE}" -c
-    "import sphinx.apidoc; print('TRUE')"
-    OUTPUT_VARIABLE SPHINX_API_DOC_FOUND
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    ERROR_QUIET)
+  FIND_SPHINX_LIBRARY("APIDOC" "sphinx.apidoc")
+  FIND_SPHINX_LIBRARY("APIDOC" "sphinx.ext.apidoc")
 
   # Get the version
   execute_process(
@@ -34,8 +39,8 @@ else()
 endif()
 
 find_package_handle_standard_args(Sphinx 
-    REQUIRED_VARS SPHINX_BUILD_FOUND SPHINX_API_DOC_FOUND
+    REQUIRED_VARS SPHINX_BUILD_FOUND SPHINX_APIDOC_FOUND
     VERSION_VAR SPHINX_VERSION
 )
  
-mark_as_advanced(SPHINX_EXECUTABLE)
+mark_as_advanced(SPHINX_BUILD_LIBRARY SPHINX_APIDOC_LIBRARY)
