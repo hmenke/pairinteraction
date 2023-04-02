d847a0cbfab1fbaaf941e5e0314f4e7d9589f891
Henri Menke <henri@henrimenke.de>
2023-04-02 21:28:46 +0200

    [ci] add diagonalization benchmark

--- pairinteraction/benchmarks/integration_bench.cpp
+++ pairinteraction/benchmarks/integration_bench.cpp
@@ -1,13 +1,14 @@
-#include "MatrixElementCache.h"
-#include "State.h"
-#include "SystemOne.h"
-#include "SystemTwo.h"
-#include "dtypes.h"
-#include "filesystem.h"
+#include "MatrixElementCache.hpp"
+#include "State.hpp"
+#include "SystemOne.hpp"
+#include "SystemTwo.hpp"
+#include "filesystem.hpp"
 
 #include <benchmark/benchmark.h>
 
 static void benchmark_integration(benchmark::State &state) {
+    using Scalar = double;
+
     // Set up cache
     fs::path path_cache = fs::create_temp_directory();
     MatrixElementCache cache(path_cache);
@@ -22,19 +23,13 @@ static void benchmark_integration(benchmark::State &state) {
         ////////////////////////////////////////////////////////////////////
 
         // Build one-atom system
-        SystemOne system_one(state_one.getSpecies(), cache);
+        SystemOne<Scalar> system_one(state_one.getSpecies(), cache);
         system_one.restrictEnergy(state_one.getEnergy() - 40, state_one.getEnergy() + 40);
         system_one.restrictN(state_one.getN() - 1, state_one.getN() + 1);
         system_one.restrictL(state_one.getL() - 1, state_one.getL() + 1);
         system_one.setEfield({{0, 0, 0.1}});
         system_one.setBfield({{0, 0, 1}});
         system_one.enableDiamagnetism(false);
-
-        // Compare current results to the reference data (the results have to be
-        // compared before diagonalization as the order of the eigenvectors is not
-        // fixed)
-        eigen_sparse_t hamiltonian_one = system_one.getHamiltonian();
-        eigen_sparse_t basis_one = system_one.getBasisvectors();
 
         // Diagonalize one-atom system
         system_one.diagonalize();
@@ -48,13 +43,13 @@ static void benchmark_integration(benchmark::State &state) {
         // eigenvectors)
         // system_one = SystemOne(state_one.species, cache); // TODO  object of type 'SystemOne'
         // cannot be assigned because its copy assignment operator is implicitly deleted
-        SystemOne system_one_new(state_one.getSpecies(), cache);
+        SystemOne<Scalar> system_one_new(state_one.getSpecies(), cache);
         system_one_new.restrictEnergy(state_one.getEnergy() - 40, state_one.getEnergy() + 40);
         system_one_new.restrictN(state_one.getN() - 1, state_one.getN() + 1);
         system_one_new.restrictL(state_one.getL() - 1, state_one.getL() + 1);
 
         // Build two-atom system
-        SystemTwo system_two(system_one_new, system_one_new, cache);
+        SystemTwo<Scalar> system_two(system_one_new, system_one_new, cache);
         system_two.restrictEnergy(state_two.getEnergy() - 2, state_two.getEnergy() + 2);
         system_two.setConservedParityUnderPermutation(ODD);
         system_two.setDistance(6);
